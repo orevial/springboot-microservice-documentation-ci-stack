2e3d81b1f782895e0767f505ae55a1ae63c20b73
lidonis <lcalhau@ippon.fr>
2017-04-10 10:28:35 +0200

    Improve publication script

--- scripts/publish-documentation.sh
+++ scripts/publish-documentation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-nginxBaseDir="/home/adminuser/workspace/html"
+nginxBaseDir="/var/www/html"
 indexFile="$nginxBaseDir/intro.html"
-baseDocumentationHtml="../base-documentation-html"
-workspace="/home/adminuser/workspace/doc-artifacts"
+baseDocumentationHtml="base-documentation-html"
+workspace="/home/lcalhau/workspace"
+artifactsDir="/home/lcalhau/doc-artifacts"
+
+mkdir -p $artifactsDir/
+
+# Copy all generated artifacts to specific folder
+for projectDir in $workspace/*; do
+  echo "Copying zip artifact from directory $projectDir"
+  cp $projectDir/target/*.zip $artifactsDir/
+done
 
 # Copy base documentation HTML to target Nginx dir
 cp -rf $baseDocumentationHtml/* $nginxBaseDir
@@ -13,23 +22,25 @@ mkdir -p "$nginxBaseDir/team1"
 echo "<li class=\"collapsible\"><span class=\"fa fa-caret-right level-1-caret\"></span><a href=\"#\">&nbsp;Team 1</a><ul class=\"sectlevel2\">" >> $indexFile
 
 # Publish all doc artifacts to target Nginx dir
-for zipfile in $workspace/*.zip; do
+for zipfile in $artifactsDir/*.zip; do
    filename=$(basename $zipfile)
-   projectName=$(echo "$filename" | sed 's/\(.*\)-[0-9]*\.[0-9]*\.[0-9]*-doc.zip/\1/')
-   projectNameWithVersion=$(echo "$filename" | sed 's/\(.*-[0-9]*\.[0-9])*\.[0-9]*\)-doc.zip/\1/')
+   projectName=$(echo "$filename" | sed 's/\(.*\)-[0-9]*\.[0-9]*\.[0-9]*-SNAPSHOT-doc.zip/\1/')
+   projectNameWithVersion=$(echo "$filename" | sed 's/\(.*-[0-9]*\.[0-9])*\.[0-9]*-SNAPSHOT\)-doc.zip/\1/')
 
     echo "Processing project $projectName"
 
     mkdir -p "$nginxBaseDir/team1/$projectName"
     unzip -oq -d "$nginxBaseDir/team1/$projectName" $zipfile
+    cp -r "$nginxBaseDir/team1/$projectName/$projectNameWithVersion" "$nginxBaseDir/team1/"
+    rm -rf "$nginxBaseDir/team1/$projectName/"
 
-    echo "<li><a href=\"team1/$projectName/$projectNameWithVersion/intro.html\" target=\"doc-iframe\">${projectName}</a></li>" >> $indexFile
+    echo "<li><a href=\"team1/$projectNameWithVersion/intro\" target=\"doc-iframe\">${projectName}</a></li>" >> $indexFile
 done
 
 # Close projects + team lists anchors
 echo "</ul></li></ul>" >> $indexFile
 
 # Close HTML file anchors
-echo "</div></div><div id=\"content\" class=\"first-level\" style=\"height:100%\"><iframe frameborder=\"0\" style=\"overflow:hidden;\" height=\"100%\" width=\"100%\" name=\"doc-iframe\" src=\"homepage.html\"></iframe></div></body></html>$
+echo "</div></div><div id=\"content\" class=\"first-level\" style=\"height:100%\"><iframe frameborder=\"0\" style=\"overflow:hidden;\" height=\"100%\" width=\"100%\" name=\"doc-iframe\" src=\"homepage.html\"></iframe></div></body></html>" >> $indexFile
 
 echo "DONE"
